fe2fd390ba13d90a2629701db8be3f2c160be963
Heba Ghazaly <hebbaghazaly@gmail.com>
2020-10-22 19:37:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,98 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+const initialCount = 10;
+const count = 30;
+const apiKey = 'ju8W-mmQ9K3JKg--iAjXxl5PJU5_jAq2kgoyG5F56vQ';
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${initialCount}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+  }
+}
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+
+  // Run function for each object in photosArray
+  photosArray.forEach(photo => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    // Create a figure caption to show location of photo
+    const figure = document.createElement('figure');
+    const figcaption = document.createElement('figcaption');
+    figcaption.classList.add('disabled');
+    if (photo.location.name !== null) {
+      figcaption.innerText = `${photo.location.name}`;
+      figcaption.href = '#';
+    }
+
+    // Event listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
+
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    item.appendChild(figure);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+    console.log(photosArray);
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getPhotos();
+  }
+});
+
+// On Load
+getPhotos();
